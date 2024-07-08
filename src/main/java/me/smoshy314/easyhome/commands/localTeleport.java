71c88749390832b29160dcb9f2afe4ed9da4936b
Josh <joshuatylermartinez@gmail.com>
2024-07-07 20:44:47 -0700

    Revert "implemented /top command"
    This reverts commit 45cdf4110e8cfc06002f580d85d323d315302418.

--- src/main/java/me/smoshy314/easyhome/commands/localTeleport.java
+++ src/main/java/me/smoshy314/easyhome/commands/localTeleport.java
@@ -1,43 +1,16 @@
 package me.smoshy314.easyhome.commands;
 
-import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.Heightmap;
-import net.minecraft.world.chunk.Chunk;
 import org.jetbrains.annotations.NotNull;
-
 
 public class localTeleport {
     public static void register(@NotNull CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(CommandManager.literal("top").executes(localTeleport::top));
-    }
-
-
-    public static int top(@NotNull CommandContext<ServerCommandSource> context)
-    {
-        if (context.getSource().getPlayer() != null)
-        {
-            ServerPlayerEntity player = context.getSource().getPlayer();
-
-            if (player.hasPermissionLevel(3)){
-                ServerWorld world = player.getServerWorld();
-                BlockPos playerPos = player.getBlockPos();
-                Chunk playerChunk = world.getChunk(playerPos);
-                int x = playerPos.getX();
-                int z = playerPos.getZ();
-                long topY =  playerChunk.sampleHeightmap(Heightmap.Type.WORLD_SURFACE, x, z);
-
-                player.teleport(world, x, topY, z, player.getYaw(), player.getPitch());
-            }
-        }
-        return Command.SINGLE_SUCCESS;
+        dispatcher.register(CommandManager.literal("top").executes(context -> {
+            context.getSource().sendFeedback(() -> Text.literal("Called /top with no arguments."), false);
+            return 1;
+        }));
     }
 }
